8ec27353372734f01cef39d28558e90f7810a9d3
Xijiang Yu <xijiang.yu@gmail.com>
2019-01-17 11:01:45 +0100

    vcf2g

--- src/vcf2g.cpp
+++ src/vcf2g.cpp
@@ -7,28 +7,35 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  int    nid(stoi(argv[1])), nlc{0};
+  int            nid{0};
   vector<int>    frq;
   vector<string> gt;
+  string         line;
 
-  for(string line; getline(cin, line);){
-    if(line[0]=='#') continue;
-    ++nlc;
+  while(getline(cin, line)) if(line[0]!='#') break;
+  {
     stringstream ss(line);
-    string aa, cg;
-    int    fq{0};
-    for(auto i=0; i<9; ++i) ss>>aa;
-    while(ss>>aa){
-      int ch;
-      ch = aa[0]-'0'+aa[2];
-      fq+=int(ch-'0');
-      cg+=ch;
+    for(string id; ss>>id; ++nid);
+    nid -= 9;
+  }
+
+  while(getline(cin, line)){
+    if(line[0]=='#') continue;
+    string idg(nid*2, '-'), aa;
+    int    i, j{0}, fq{0};
+    stringstream ss(line);
+    for(i=0; i<9; ++i) ss>>aa;
+    for(i=0; i<nid; ++i){
+      ss>>aa;
+      idg[j++] = aa[0];
+      idg[j++] = aa[2];
     }
-    gt.push_back(cg);
+    for(auto ch:idg) if(ch=='1') ++fq;
+    gt.push_back(idg);
     frq.push_back(fq);
   }
 
-  cout<<nid<<' '<<nlc<<'\n';
+  cout<<nid<<' '<<frq.size()<<'\n';
   for(auto&f:frq) cout<<f<<'\n';
   for(auto&s:gt ) cout<<s<<'\n';
   return 0;
