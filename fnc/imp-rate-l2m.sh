1b35654b64b10da94fbd782a1eea0616990de550
Xijiang Yu <xijiang.yu@nmbu.no>
2019-08-15 22:42:52 +0200

    lmr-3

--- fnc/imp-rate-l2m.sh
+++ fnc/imp-rate-l2m.sh
@@ -11,10 +11,9 @@ prepare-a-working-directory(){
     work=$base/work/l2m.rate
     mkdir -p $work
     cd work
-    ln -sf $base/work/a17k.g/md.{1..26}.vcf.gz .
-    # The SNP on the 7k chip
-    cat $maps/7327.map |
-	gawk '{print $2}' >ld.snp
+    for chr in {1..26}; do
+	ln -sf $base/work/a17k.g/imp.$chr.vcf.gz md.$chr.vcf.gz
+    done
 }
 
 
@@ -25,8 +24,9 @@ make-reference(){
              ne=$ne \
              out=ref.$chr
     done
+    # The SNP on the 7k chip
     cat $maps/7327.map | 
-	gawk '{print $2, $1, $4}' > ld.map
+	gawk '{print $2, $1, $4}' > ld.snp
 
     zcat md.{1..26}.vcf.gz |
 	grep -v \# |
@@ -76,7 +76,7 @@ sample-n-mask-n-impute(){
 test-lmr(){
     prepare-a-working-directory
 
-    make-reference
+#    make-reference
 
 }
 
